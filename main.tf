e81d85088d5f7a1b4c5ecbebf7dc6b1d8f589d73
Syed-0140 <116809024+Syed-0140@users.noreply.github.com>
2022-12-16 19:33:39 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "AKIA2W3RP4ZMTO4PZLO4"
   secret_key = "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"
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "one" {
   count =2
@@ -9,7 +9,7 @@ resource "aws_instance" "one" {
   instance_type = "t2.micro"
   
   tags = {
-    Name = "projectinstance"
+    Name = "terrainstance"
   }
 }
 resource "aws_s3_bucket" "two" {
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "two" {
 }
 resource "aws_ebs_volume" "three" {
   availability_zone = "us-east-1a"
-  size              = 40
+  size              = 20
   tags = {
     Name = "HelloWorld"
   }
