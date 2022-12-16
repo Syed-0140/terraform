cb2de5be3242cb0d1f8c659aa7ff993ac4c62976
Syed-0140 <116809024+Syed-0140@users.noreply.github.com>
2022-12-16 16:48:38 +0530

    commited

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  access_key = "AKIA2W3RP4ZMVKPQMOGX"
+  secret_key = "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"
+  region = "us-east-1"
+}
+resource "aws_instance" "one" {
+  count =2
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "projectinstance"
+  }
+}
+resource "aws_s3_bucket" "two" {
+  bucket = "syed buket"
+ tags = {
+    Name        = " syed bucket"
+  }
+}
+resource "aws_ebs_volume" "three" {
+  availability_zone = "us-east-1a"
+  size              = 40
+  tags = {
+    Name = "HelloWorld"
+  }
+}
