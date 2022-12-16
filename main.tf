57ab7071db040652d55a7b90dbb3ef8c812fc782
Syed-0140 <116809024+Syed-0140@users.noreply.github.com>
2022-12-16 19:47:32 +0530

    commit

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  count =2
-  ami           = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
+  ami           = "ami-064bd2d44a1d6c097"
+  instance_type = "c5a.4xlarge"
   
   tags = {
     Name = "terrainstance"
