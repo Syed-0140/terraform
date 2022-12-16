4196c68a67ef85958a144a4af7136626db7493f6
Syed-0140 <116809024+Syed-0140@users.noreply.github.com>
2022-12-16 18:52:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 resource "aws_instance" "one" {
   count =2
-  ami           = "ami-0b0dcb5067f052a63"
+  ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
   
   tags = {
