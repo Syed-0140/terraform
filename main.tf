97f78b709eb6f8da41d2de92bd09133711b4025f
Syed-0140 <116809024+Syed-0140@users.noreply.github.com>
2022-12-30 11:50:31 +0530

    commits

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 resource "aws_instance" "one" {
   ami           = "ami-064bd2d44a1d6c097"
-  instance_type = "c5a.4xlarge"
+  instance_type = "t2.micro"
   availability_zone = "us-east-1b"
   
   tags = {
